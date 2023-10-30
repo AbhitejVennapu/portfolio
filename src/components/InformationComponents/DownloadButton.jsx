89c960147e430f65c5b5fd06963e47f2aeb8c760
Abhitej Vennapu <vennapu@rhrk.uni-kl.de>
2023-10-30 15:37:58 +0100

    changed anchor for delete

--- src/components/InformationComponents/DownloadButton.jsx
+++ src/components/InformationComponents/DownloadButton.jsx
@@ -1,32 +1,39 @@
 import React from 'react'
 import { Box, Button } from '@mui/material'
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
-
+import ResumePDF from '../../assets/Resume.pdf';
 const DownloadButton = () => {
 
-  function handleDownloadClick() {
+  // function handleDownloadClick() {
 
-    const fileUrl = '/Resume.pdf';
+  //   const fileUrl = {ResumePDF};
   
-    const link = document.createElement('a');
-    link.href = fileUrl;
-    link.download = 'CV_Abhitej'; 
-    link.target = '_blank'; 
+  //   const link = document.createElement('a');
+  //   link.href = fileUrl;
+  //   link.download = 'CV_Abhitej'; 
+  //   link.target = '_blank'; 
   
-    document.body.appendChild(link);
-    link.click();
+  //   document.body.appendChild(link);
+  //   link.click();
   
-    document.body.removeChild(link);
-  }
+  //   document.body.removeChild(link);
+  // }
 
   
   return (
     <>
       <Box sx = {{ alignItems: 'right', textAlign: 'right'}}>
+      <a
+        href={ResumePDF}
+        download="AbhitejCV"
+        target="_blank"
+        rel="noreferrer"
+      >  
         <Button>
-          <FileDownloadOutlinedIcon onClick = {handleDownloadClick} 
+          <FileDownloadOutlinedIcon 
         sx = {{color: (theme) => (theme.palette.mode === "dark" ? "#FFF" : "#000")}}/>
         </Button>
+        </a>
       </Box>
     </>
   )
