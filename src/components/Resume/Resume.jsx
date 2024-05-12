906f29e941f19e3727df430fcb819687511747fe
Abhitej Vennapu <vennapu@rhrk.uni-kl.de>
2024-05-12 12:56:40 +0200

    fix resume file

---
+++ src/components/Resume/Resume.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+import Education from './Education'
+
+const Resume = () => {
+  return (
+    <Stack spacing={2}>
+      <Education></Education>
+    </Stack>
+  )
+}
+
+export default Resume
--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,20 +1,20 @@
-import Grid from '@mui/material/Grid';
-import Profile from '../components/Profile';
-import Information from '../components/Information';
+import Grid from '@mui/material/Grid'
+import Profile from '../components/Profile'
+import Information from '../components/Information'
 
 const LandingPage = () => {
-  return (
-    <>
-        <Grid container>
-            <Grid item sm = {6} xs = {12}>
-                <Profile></Profile>
+    return (
+        <>
+            <Grid container>
+                <Grid item sm={6} xs={12}>
+                    <Profile></Profile>
+                </Grid>
+                <Grid item sm={6} xs={12}>
+                    <Information></Information>
+                </Grid>
             </Grid>
-            <Grid item sm = {6} xs = {12}>
-                <Information></Information>
-            </Grid>
-        </Grid>
-    </>
-  )
+        </>
+    )
 }
 
 export default LandingPage
